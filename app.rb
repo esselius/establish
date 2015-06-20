4e097e612a6946e50cab676dd7ef263e50956db3
Peter Esselius <peter@saltside.se>
2015-06-21 01:58:51 +0200

    Remove HostGroup class

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require 'concord'
 
 require 'aws-sdk'
 
-require_relative 'app/host_group'
 require_relative 'app/auto_scaling_group'
 require_relative 'app/auto_scaling_client'
 require_relative 'app/launch_configuration'
